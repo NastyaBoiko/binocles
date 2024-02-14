4a9514175abda47db294aba8dbbb78811bb671bd
Nastya <killer99whale@mail.ru>
2024-02-14 13:35:19 +0300

    task 5

--- src/Config/routes.php
+++ src/Config/routes.php
@@ -2,5 +2,7 @@
 
 return [
     '~^hello/(.*)$~' => [\Src\Controllers\MainController::class, 'sayHello'],
+    '~^products/all$~' => [\Src\Controllers\ProductController::class, 'all'],
+    '~^product/(.*)$~' => [\Src\Controllers\ProductController::class, 'show'],
     '~^$~' => [\Src\Controllers\MainController::class, 'main'],
 ];
---
+++ src/Views/Main/product.php
@@ -0,0 +1,4 @@
+
+<h1>Продукт <?= $product['name']?> из категории <?= $product['category']?></h1>
+<p>Цена: <?= $product['price']?>$, количество: <?= $product['quantity']?></p>
+<p>Стоимость (цена * количество): <?= $product['price'] * $product['quantity']?>$</p>
