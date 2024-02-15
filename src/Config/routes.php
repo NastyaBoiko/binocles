7074fc16d2260934e96de1997d0daadb8e0e96ce
nastya <killer99whale@mail.ru>
2024-02-15 11:08:35 +0300

    articles

--- src/Config/routes.php
+++ src/Config/routes.php
@@ -4,6 +4,7 @@ return [
     '~^hello/(.*)$~' => [\Src\Controllers\MainController::class, 'sayHello'],
     '~^products/all$~' => [\Src\Controllers\ProductController::class, 'all'],
     '~^product/(.*)$~' => [\Src\Controllers\ProductController::class, 'show'],
-    '~^articles/all$~' => [\Src\Controllers\ArticlesController::class, 'main'],
+    '~^articles/all$~' => [\Src\Controllers\ArticlesController::class, 'all'],
+    '~^articles/(\d+)$~' => [\Src\Controllers\ArticlesController::class, 'view'],
     '~^$~' => [\Src\Controllers\MainController::class, 'main'],
 ];
--- src/Controllers/ArticlesController.php
+++ src/Controllers/ArticlesController.php
@@ -8,7 +8,22 @@ use Src\Services\Db;
 class ArticlesController extends Controller
 {
 
-    public function main()
+    public function view(int $articleId)
+    {
+        $result = $this->db->query(
+            'SELECT * FROM `articles` WHERE id = :id;',
+            [':id' => $articleId]
+        );
+
+        if ($result === []) {
+            // Здесь обработка ошибки
+            return;
+        }
+        
+        $this->view->renderHtml('Articles/view.php', ['article' => $result[0]]);
+    }
+
+    public function all()
     {
         $articles = $this->db->query('SELECT * FROM `articles`;');
         $this->view->renderHtml('Articles/articles.php', ['articles' => $articles]);
